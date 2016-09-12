f47590f161dafe03db49aac383883f8eeb630286
Pelle <Pelle@LAPTOP-T69PBCOE>
2016-09-12 09:39:46 +0200

    Add project files.

---
+++ 03_Simon_Says/Simon.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _03_Simon_Says
+{
+    public class Simon
+    {
+        internal string StartOfWord(string v1, int v2)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        internal string FirstWord(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string Titleize(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string Echo(string v)
+        {
+            string b = v.ToLower();
+            return b;
+            
+        }
+
+        internal string Repeat(string v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string Shout(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Repeat(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
