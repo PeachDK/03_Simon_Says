dd6551ade319665b6ee92c14f7fc746e272589da
Pelle <Pelle@LAPTOP-T69PBCOE>
2016-09-12 13:08:06 +0200

    Done with assigment.

--- 03_Simon_Says/Simon.cs
+++ 03_Simon_Says/Simon.cs
@@ -1,24 +1,52 @@
 ﻿using System;
+using System.Globalization;
+
 
 namespace _03_Simon_Says
 {
     public class Simon
     {
         internal string StartOfWord(string v1, int v2)
-        {            
-            return v1.Substring(0,v2);            
-         
+        {
+            return v1.Substring(0, v2);
+
         }
 
         internal string FirstWord(string v)
-        {           
+        {
             string[] s = v.Split();
             return s[0];
         }
 
         internal string Titleize(string v)
         {
-            throw new NotImplementedException();
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+            TextInfo textInfo = cultureInfo.TextInfo;
+
+            //Array contains word that should not be capitalized.
+            string[] fy = { "and", "over", "the" };
+
+            //inputs string v into a array. Using build in method spilt.
+            string[] s = v.Split();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[0] != null)
+                {
+                    s[0] = textInfo.ToTitleCase(s[0]);
+                }
+
+                bool a = Array.Exists(fy, element => element == s[i]);
+                if (a == false)
+                {
+                    s[i] = textInfo.ToTitleCase(s[i]);
+                }
+
+            }
+
+
+            return string.Join(" ", s);
+
         }
 
         internal string Echo(string v)
@@ -34,7 +62,7 @@ namespace _03_Simon_Says
             string s = "";
 
             for (int i = v2; i > 0; i--)
-            { 
+            {
 
                 s += b + " ";
             }
